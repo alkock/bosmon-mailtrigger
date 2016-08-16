e3c8a22242c00085a7b9a54a20898c4f3e520296
Johannes Homuth <johannes.homuth@sourcepark.de>
2016-08-16 10:23:44 +0200

    added "" between username and passwort

--- src/main/java/de/ffwbeetzsommerfeld/bosmon/mailreader/util/Executor.java
+++ src/main/java/de/ffwbeetzsommerfeld/bosmon/mailreader/util/Executor.java
@@ -19,7 +19,7 @@ public enum Executor {
         public String[] getCommand(Alarm alarm) {
             String[] curlCommand = {Config.get(Config.KEY_CURL_EXE),
                 "--basic",
-                String.format("-u %s:%s", Config.get(Config.KEY_BOSMON_USER),Config.get(Config.KEY_BOSMON_PASS)),
+                String.format("-u \"%s:%s\"", Config.get(Config.KEY_BOSMON_USER),Config.get(Config.KEY_BOSMON_PASS)),
                 String.format("%s:%s/telegramin/%s/input.xml", Config.get(Config.KEY_BOSMON_SERVER_NAME), Config.get(Config.KEY_BOSMON_SERVER_PORT), Config.get(Config.KEY_BOSMON_CHANNEL_NAME)),
                 String.format("--data \"type=pocsag&address=%s&flags=0&function=b&message=%s\"", alarm.getRic(), alarm.getMessage()),
                 "-vk"};
